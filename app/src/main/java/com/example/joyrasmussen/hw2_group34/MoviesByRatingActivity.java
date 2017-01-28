ab1393cc7fcdecb507d95a4f18fefa5c763d7264
Clay Holt <clayholt@gmx.com>
2017-01-28 17:25:47 -0500

    View activities done

---
+++ app/src/main/java/com/example/joyrasmussen/hw2_group34/MoviesByRatingActivity.java
@@ -0,0 +1,13 @@
+package com.example.joyrasmussen.hw2_group34;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoviesByRatingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies_by_rating);
+    }
+}
---
+++ app/src/main/java/com/example/joyrasmussen/hw2_group34/MoviesByYearActivity.java
@@ -0,0 +1,13 @@
+package com.example.joyrasmussen.hw2_group34;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoviesByYearActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movies_by_year);
+    }
+}
